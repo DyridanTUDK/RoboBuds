89220ee44797a3739ee56bd3e8aa8bee4a8e4104
DyridanTUDK <50053867+DyridanTUDK@users.noreply.github.com>
2020-09-28 05:17:06 +0600

    Add files via upload

---
+++ build/precache-manifest.a8419ec0a7a18a54146cb6a83edb3df5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f894165fb17f56f8aa9c58cac58022",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e2a23be948c4ba49a9c0",
+    "url": "/static/css/main.956fd9a3.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a23be948c4ba49a9c0",
+    "url": "/static/js/main.4d79cb28.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "61be1af1af2ed8629c1595102f8e4fe5",
+    "url": "/static/media/SansSerifShaded.61be1af1.woff"
+  }
+]);
